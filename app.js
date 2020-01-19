3f3b76010d52e8db16e721b84c0b416d789401fd
lisongyu <269876940@qq.com>
2020-01-19 12:20:57 +0800

    提交路由优化配置

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const Koa = require('koa')
-
+const requireDirectory=require('require-directory')
 const app = new Koa();
+const Router=require('koa-router')
 
-const book = require('./api/v1/book')
+// const book = require('./api/v1/book')
 
-const classic = require('./api/v1/classic')
+// const classic = require('./api/v1/classic')
+requireDirectory(module, './app/api', {
+  visit: whenloadModule
+});
 
+
+
+function whenloadModule(obj) {
+  if (obj instanceof Router) {
+    app.use(obj.routes())
+  }
+  
+}
 //应用程序对象 中间件
 
 //发送HTTP KOA 接收HTTp
 
 //函数
 
-
-
-
-app.use(book.routes())
-app.use(classic.routes())
 
 //注册 中间件
 
